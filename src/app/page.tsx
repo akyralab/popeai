55d42097a3f614656031f34307cbdb7a8dddb5cc
Aridan Pantoja <aridanpantoja@gmail.com>
2024-11-13 21:20:17 -0300

    feat: enhance root layout and home page structure
    - Add responsive container to body
    - Implement basic home page content with GitHub link and description
    - Import and use utility functions and site config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import { badgeVariants } from '@/components/ui/badge'
+import { siteConfig } from '@/config/site'
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <>
-      <h1>PopeAI</h1>
+      <section className="mx-auto my-16 flex max-w-prose flex-col items-center justify-center gap-4 text-center">
+        <Link href={siteConfig.links.github} className={badgeVariants()}>
+          Star on Github ⭐
+        </Link>
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Build Your Chatbot with Ease
+        </h1>
+        <p className="leading-7">{siteConfig.description}</p>
+      </section>
     </>
   )
 }
