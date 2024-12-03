6ea835007c1b7e35c7089dea351771cb7ebd6db7
kwojtyla <karolwojtyla360@gmail.com>
2024-12-03 11:07:24 -0300

    feat: add ai model selection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+import { GoogleIcon, OpenAiIcon } from '@/assets/icons'
 import { badgeVariants } from '@/components/ui/badge'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 import { siteConfig } from '@/config/site'
 import Link from 'next/link'
 
@@ -13,6 +21,25 @@ export default function Home() {
           Build Your Chatbot with Ease
         </h1>
         <p className="leading-7">{siteConfig.description}</p>
+        <Select>
+          <SelectTrigger className="w-[240px] shadow-none">
+            <SelectValue placeholder="Select your AI model" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="openai">
+              <div className="flex items-center gap-2">
+                <OpenAiIcon size={16} />
+                <span>OpenAI</span>
+              </div>
+            </SelectItem>
+            <SelectItem value="gemini">
+              <div className="flex items-center gap-2">
+                <GoogleIcon size={16} />
+                <span>Gemini</span>
+              </div>
+            </SelectItem>
+          </SelectContent>
+        </Select>
       </section>
     </>
   )
